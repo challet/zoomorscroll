84b1c394f7d09a8a141603d9a63dfda79ee69a83
Clément Hallet <clement@challet.eu>
2014-07-16 13:41:06 +0200

    poc 2

--- src/zoomorscroll.js
+++ src/zoomorscroll.js
@@ -6,38 +6,98 @@
  * Licensed under the MIT license.
  */
 
-(function ($) {
 
-  $.zoomorscroll.maps = $();
+
+(function ($) {
+  
+  // define a custom 'capture' event type (TODO make a specific plugin ?)
+  $.event.special.capture = {
+    noBubble: true, // used when a manual 'trigger' is fired
+    setup: function(data, namespaces, eventHandle ) {
+      return true; // no need to set up a native listener
+    },
+    teardown: function(data, namespaces, eventHandle ) {
+      return true; // no need to set up a native listener
+    },
+    add: function(handleObj) {
+      this.addEventListener( handleObj.namespace, handleObj.handler, true ); // use capture
+    },
+    remove: function(handleObj) {
+      this.removeEventListener( handleObj.namespace, handleObj.handler , true ); // use capture
+    },
+    handle: function() {
+      console.log('arguments');
+    }
+  };
+  
+  var initialized = false, no_scroll_timer = null;
+  
+  // Collection method.
+  $.fn.zoomorscroll = function (options) {
+    options = $.extend({}, $.zoomorscroll.options, $(this).data('zoomorscroll-options'), options);
+    $(this).data('zoomorscroll-options', options).data('zoomorscroll-holding', true);
+    $.merge($.zoomorscroll.elements, $(this));
+    $.zoomorscroll();
+    return this;
+  };
+  
+  // Static method, handles the scroll events
+  $.zoomorscroll = function () {
+    
+    if(initialized) {
+      return;
+    }
+    
+    $(document).on('capture.wheel', function(event) {
+      
+      if(event.eventPhase != Event.CAPTURING_PHASE) {
+        return;
+      }
+      
+      if(no_scroll_timer) {
+        window.clearTimeout(no_scroll_timer);
+      }
+      
+      var zoomable_target = $.zoomorscroll.elements.filter(event.target);
+      // start to check when to stop the holding
+      if(zoomable_target.length && !zoomable_target.data('zoomorscroll-undercheck')) {
+        zoomable_target.data('zoomorscroll-undercheck', true);
+        var target_options = zoomable_target.data('zoomorscroll-options');
+        var unhold_it = function() { 
+          zoomable_target.data('zoomorscroll-holding', false);
+        }
+        //if(target_options.reset.no_scroll_timer) {
+        //  no_scroll_timer = window.setTimeout(unhold_it, target_options.reset.no_scroll_timer);
+        //}
+        if(target_options.reset.click) {
+          zoomable_target.one('click', unhold_it);
+        }
+      }
+      
+      
+      if(zoomable_target.length && zoomable_target.data('zoomorscroll-holding')) {
+        event.stopPropagation();
+      } 
+      if(!zoomable_target.length) {
+        // reset all holdings
+        $.zoomorscroll.elements.data('zoomorscroll-undercheck', false);
+        $.zoomorscroll.elements.data('zoomorscroll-holding', true)
+      }
+      
+    });
+    
+    
+  };
 
   $.zoomorscroll.options = {
     // conditions to stop the plugin, and let the user zoom the map
     reset : {
-      no_scroll_timer: 200, // ms
-      click: true,
-      around_radius: 50 // px
+      no_scroll_timer: 2000, // ms
+      click: true
     }
   };
   
-  var initalized = false;
+  $.zoomorscroll.elements = $();
   
-  // Collection method.
-  $.fn.zoomorscroll = function (options) {
-    return this.each(function () { 
-      $.zoomorscroll.maps.push(
-        $(this).data('zoomorscroll-options', $.extend({}, default_options, options))
-      );
-    }
-    $.zoomorscroll.maps = $.unique($.zoomorscroll.maps)
-    if (!initalized) $.zoomorscroll();
-  };
-
-  // Static method, handles the scroll events
-  $.zoomorscroll = function (options) {
-    $(document).on('mousewheel', function(event) {
-      event.stopPropagation();
-    }
-  };
-
 
 }(jQuery));
--- test/zoomorscroll_test.js
+++ test/zoomorscroll_test.js
@@ -20,43 +20,37 @@
       throws(block, [expected], [message])
   */
 
-  module('jQuery#awesome', {
+  $(document).ready(function() {
+    // faking a zoom handler
+    $('#fake_map').on('wheel', function(event) {
+      console.log('zooming');
+      // do zoom things
+      event.preventDefault();
+      event.stopPropagation(); // as seen on https://github.com/Leaflet/Leaflet/blob/master/src/dom/DomEvent.js#L166
+    });
+
+    $(document).ready(function() {
+      $('#fake_map').zoomorscroll();
+    });
+  });
+
+  module('jQuery#zoomorscroll', {
     // This will run before each test in this module.
     setup: function() {
       this.elems = $('#qunit-fixture').children();
     }
   });
 
-  test('is chainable', function() {
-    expect(1);
-    // Not a bad test to run on collection methods.
-    strictEqual(this.elems.awesome(), this.elems, 'should be chainable');
+  //test('is chainable', function() {
+  //  expect(1);
+  //  // Not a bad test to run on collection methods.
+  //  strictEqual(this.elems.zoomorscroll(), this.elems, 'should be chainable');
+  //});
+
+  test('stores options in the elements', function() {
+    
+    //$('#qunit-fixture').zoomorscroll();
   });
 
-  test('is awesome', function() {
-    expect(1);
-    strictEqual(this.elems.awesome().text(), 'awesome0awesome1awesome2', 'should be awesome');
-  });
-
-  module('jQuery.awesome');
-
-  test('is awesome', function() {
-    expect(2);
-    strictEqual($.awesome(), 'awesome.', 'should be awesome');
-    strictEqual($.awesome({punctuation: '!'}), 'awesome!', 'should be thoroughly awesome');
-  });
-
-  module(':awesome selector', {
-    // This will run before each test in this module.
-    setup: function() {
-      this.elems = $('#qunit-fixture').children();
-    }
-  });
-
-  test('is awesome', function() {
-    expect(1);
-    // Use deepEqual & .get() when comparing jQuery objects.
-    deepEqual(this.elems.filter(':awesome').get(), this.elems.last().get(), 'knows awesome when it sees it');
-  });
 
 }(jQuery));
